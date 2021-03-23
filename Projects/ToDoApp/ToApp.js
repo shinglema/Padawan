55c66bea9ca36b02b26059839b2df34cd8e3bb19
shinglema <50598577+shinglema@users.noreply.github.com>
2021-03-23 16:46:24 -0400

    Update ToApp.js

--- Projects/ToDoApp/ToApp.js
+++ Projects/ToDoApp/ToApp.js
@@ -9,12 +9,27 @@ let html_for_choices = `<label for="Choice">Notes:</label>
 
 
 function addTodo(text) {
-    var mynewdate = new Date();
+      hours = mynewdate.getHours();
+    minutes = mynewdate.getUTCMinutes();
+    seconds = mynewdate.getSeconds();
+    timeString = hours.toString().padStart(2, '0') +
+        ':' + minutes.toString().padStart(2, '0') +
+        ':' + seconds.toString().padStart(2, '0');
+
+
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+
+
+
 
     const todo = {
         id: Date.now(),
         datefull: mynewdate,
-        datelogged: mynewdate.getMonth() + '/' + mynewdate.getDate() + '/' + mynewdate.getFullYear() + '@' + mynewdate.getHours() + ':' + mynewdate.getMinutes() + '.' + mynewdate.getSeconds(),
+        datelogged: mynewdate.getMonth() + '/' + mynewdate.getDate() + '/' + mynewdate.getFullYear() + ' @ ' + strTime,
         checked: false,
         taskItem: text,
         taskNotes: "",
@@ -26,7 +41,6 @@ function addTodo(text) {
     console.log(todoItems);
     //renderTodo(todo);
     renderTodo_New();
-
 }
 
 
